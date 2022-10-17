15aec86932cae19cf127b159b9b11072596ced39
Rizvan <rizv0001@easv365.dk>
2022-10-17 12:56:56 +0200

    C# project

---
+++ C#/WordCounterDemo/WordCounterDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WordCounterDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new App().Run();
+        }
+    }
+}
