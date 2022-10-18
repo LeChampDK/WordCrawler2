1d2362f71cb09f7387dca59d3af381fd3a6209ae
René Jørgensen <rene686f@easv365.dk>
2022-10-18 11:05:44 +0200

    Update

--- Java/src/Crawler.java
+++ Java/src/Crawler.java
@@ -2,8 +2,10 @@ import Models.BEDocument;
 import jdk.jshell.spi.ExecutionControl;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.net.URI;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -17,14 +19,15 @@ public class Crawler {
 
     public Set<String> ExtractWordsInFile(String file) throws IOException {
         String regex = "([^a-zA-Z']+)'*\\1*";
-        Path path = Paths.get(URI.create(file));
-        List<String> lines = Files.readAllLines(path);
+        Path path = Paths.get(file);
+//        Path path = Paths.get(URI.create(file));
 
         Set<String> res = new HashSet<String>();
-        List<String> content = Files.readAllLines(path);
 
-        for (String line: content) {
-            for (String word: line.split(regex)
+        List<String> content = Files.readAllLines(path, StandardCharsets.UTF_8);
+
+        for (String line : content) {
+            for (String word : line.split(regex)
             ) {
                 res.add(word);
             }
@@ -33,31 +36,53 @@ public class Crawler {
         return res;
     }
 
-    public void CrawlSequentially(File folder) throws IOException {
-        for (final File fileEntry : folder.listFiles()) {
-            if (fileEntry.isDirectory()) {
-                CrawlSequentially(fileEntry);
-            } else {
-                BEDocument doc = new BEDocument();
-                doc.setmId(documents.size() + 1);
-                doc.setmUrl(fileEntry.getName());
-                doc.setmIdxTimel(LocalDateTime.now().toString());
-                doc.setmCreationTime(Files.getAttribute(fileEntry.toPath(), "creationTime").toString());
-                documents.add(doc);
+    public void CrawlSequentially(String folder) throws IOException {
 
-                Set<String> foundWords = ExtractWordsInFile(fileEntry.getName());
-                for(String word : foundWords){
-                    if(!words.containsKey(word)){
-                        words.put(word, 1);
-                    } else {
-                        words.put(word, words.get(word) + 1);
+        File dir = new File(folder);
+        File[] files = dir.listFiles();
+
+        if (files != null && files.length > 0) {
+            for (File file : files) {
+                // Check if the file is a directory
+                if (file.isDirectory()) {
+                    // We will not print the directory name, just use it as a new
+                    // starting point to list files from
+                    CrawlSequentially(file.getAbsolutePath());
+                } else {
+                    if (!file.getPath().endsWith(".txt")) {
+                        continue;
                     }
+
+                    BEDocument doc = new BEDocument();
+                    doc.setmId(documents.size() + 1);
+                    doc.setmUrl(file.getName());
+                    doc.setmIdxTimel(LocalDateTime.now().toString());
+                    doc.setmCreationTime(Files.getAttribute(file.toPath(), "creationTime").toString());
+                    documents.add(doc);
+
+                    String absolutePath = file.getAbsolutePath();
+
+                    Set<String> foundWords = ExtractWordsInFile(absolutePath);
+                    for (String word : foundWords) {
+                        if (!words.containsKey(word)) {
+                            words.put(word, 1);
+                        } else {
+                            words.put(word, words.get(word) + 1);
+                        }
+
+                    }
+
+                    // We can use .length() to get the file size
+                    System.out.println(file.getName() + " (size in bytes: " + file.length() + ")");
                 }
             }
         }
+
+        System.out.println(words.get("WASHINGTON"));
+        System.out.println(documents.size());
     }
 
-    public void CrawlParallel(){
+    public void CrawlParallel() {
         //throw new ExecutionControl.NotImplementedException();
     }
 }
--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,10 +1,18 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+
 public class Main {
-    private Crawler crawler = new Crawler();
-    private String root;
+    private static Crawler crawler = new Crawler();
+    private static String  root;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello world!");
-        
+        root = System.getProperty("user.dir") + "\\arnold-j\\arnold-j";
+        File folder = new File(root);
+        String folderDir = folder.getPath();
+        System.out.println(folderDir);
 
+        crawler.CrawlSequentially(folderDir);
     }
 }
